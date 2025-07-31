87f0eb3e577bfdeb7f0a161b6d36768fb0cfb3f5
Максим Блажко <maxim007blazhko@gmail.com>
2025-07-31 10:31:16 +0300

    Updated creds

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import numpy as np
 import requests
 import json
 import torch
+import os
 from io import BytesIO
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -23,7 +24,22 @@ model, processor = load_model()
 @st.cache_resource
 def load_sheet():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('F:\Rmt\Style\\feed\google_creds.json', scope)
+    creds = {
+        "type": "service_account",
+        "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
+        "private_key_id": os.environ.get("GOOGLE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("GOOGLE_PRIVATE_KEY"),
+        "client_email": os.environ.get("GOOGLE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get("GOOGLE_CLIENT_X509_CERT_URL"),
+        "universe_domain": "googleapis.com"
+    }
+    with open('google_creds.json', 'w') as file:
+        json.dump(creds, file)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('google_creds.json', scope)
     client = gspread.authorize(creds)
     sheet = client.open("StyleApp test feed").sheet1
     return sheet
